67d5bfb4eedbdb6120b0735186ea937f2ce83a0f
Lev Chelyadinov <leva181777@gmail.com>
2023-06-16 21:03:29 +0300

    Create a tray icon and have it spawn the window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{ActivationPolicy, SystemTray, SystemTrayEvent};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +10,33 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
+    let tray = SystemTray::new();
+    let mut app = tauri::Builder::default()
+        .system_tray(tray)
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => {
+                tauri::WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
+                    .title("WHU")
+                    .build()
+                    .expect("cannot build window");
+            }
+            _ => {}
+        })
+        .setup(|app| {
+            app.hide().expect("cannot hide app");
+            Ok(())
+        })
+        .build(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    #[cfg(target_os = "macos")]
+    app.set_activation_policy(ActivationPolicy::Accessory);
+
+    app.run(|_app_handle, event| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+            api.prevent_exit();
+        }
+        _ => {}
+    });
 }
