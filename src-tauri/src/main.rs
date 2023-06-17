27883f0d65b6aac6369035a68ceb5c13e06d532f
Lev Chelyadinov <leva181777@gmail.com>
2023-06-17 21:39:52 +0300

    Add tray items to show time until a break and to skip to the next one

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,28 +1,91 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{ActivationPolicy, SystemTray};
-use tokio::time::{Duration, interval, MissedTickBehavior};
+use futures_lite::future::race;
+use std::future::Future;
+use tauri::{
+    ActivationPolicy, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu,
+    SystemTrayMenuItem,
+};
+use tokio::{
+    sync::{mpsc, watch},
+    time::{interval, Duration, Instant, MissedTickBehavior},
+};
 
 fn main() {
-    let tray = SystemTray::new();
+    let (tx, rx) = watch::channel::<Instant>(Instant::now());
+    let (tx2, mut rx2) = mpsc::unbounded_channel::<Option<Instant>>();
+    let skip_to_next_break =
+        CustomMenuItem::new("skip_to_next_break".to_string(), "Skip to next break");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let time_until_break =
+        CustomMenuItem::new("time_until_break".to_string(), "Time until break").disabled();
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(time_until_break)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(skip_to_next_break)
+        .add_item(quit);
+    let tray = SystemTray::new().with_menu(tray_menu);
     let mut app = tauri::Builder::default()
         .system_tray(tray)
+        .on_system_tray_event(move |app_handle, event| match event {
+            SystemTrayEvent::LeftClick { .. } => {
+                let last_instant = *rx.borrow();
+                match last_instant.checked_add(Duration::from_secs(5 * 60)) {
+                    Some(next_break_instant) => {
+                        let time_to_next_break = next_break_instant - Instant::now();
+                        let item_handle = app_handle.tray_handle().get_item("time_until_break");
+                        item_handle
+                            .set_title(format!(
+                                "Time until break: {} seconds",
+                                time_to_next_break.as_secs()
+                            ))
+                            .expect("cannot set remaining time until next break in tray");
+                    }
+                    None => {}
+                }
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "skip_to_next_break" => {
+                    tx2.send(None).expect("cannot send skip to next break");
+                }
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .setup(|app| {
             let app = app.handle();
+
             tauri::async_runtime::spawn(async move {
                 let mut interval = interval(Duration::from_secs(5 * 60));
                 interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
                 interval.tick().await;
 
                 loop {
-                    interval.tick().await;
+                    match race(wrap_with_options(interval.tick()), rx2.recv())
+                        .await
+                        .unwrap()
+                    {
+                        Some(tick) => {
+                            tx.send(tick).expect("cannot send last tick");
+                        }
+                        None => {
+                            tx.send(Instant::now()).expect("cannot send last tick");
+                            interval.reset();
+                        }
+                    }
                     tauri::WindowBuilder::new(
                         &app,
                         "main",
                         tauri::WindowUrl::App("index.html".into()),
                     )
                     .title("WHU")
+                    .fullscreen(true)
+                    .always_on_top(true)
+                    .closable(false)
                     .build()
                     .expect("cannot build window");
                 }
@@ -42,4 +105,10 @@ fn main() {
         }
         _ => {}
     });
+}
+
+async fn wrap_with_options<InnerType>(
+    future: impl Future<Output = InnerType>,
+) -> Option<Option<InnerType>> {
+    Some(Some(future.await))
 }
